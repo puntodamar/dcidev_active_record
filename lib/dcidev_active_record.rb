70eef9f25768da5936d035cf0a47e85d26f167b6
puntodamar <punto.damar@ti.ukdw.ac.id>
2022-01-05 00:04:23 +0700

    update eval

--- lib/dcidev_active_record.rb
+++ lib/dcidev_active_record.rb
@@ -18,33 +18,10 @@ module DcidevActiveRecord
     def postgresql_time_builder(field)
       "#{field}::TIMESTAMPTZ AT TIME ZONE '#{Time.zone.now.formatted_offset}'"
     end
-
-    def set_order
-      return unless self.class.column_names.include?("view_order")
-      if self.view_order.present?
-        self.reorder
-      else
-        self.view_order = self.class.where.not(id: self.id).count + 1
-        self.save
-      end
-    end
-
-    def reorder
-      return unless self.class.column_names.include?("view_order")
-      return unless self.class.where(view_order: self.view_order).where.not(id: self.id).present?
-      self.class.order(view_order: :asc, updated_at: :desc).each.with_index(1) do |f, i|
-        f.update(view_order: i)
-      end
-    end
   end
 
-  ActiveRecord::Base.class_eval do
-    scope :between_date, -> (column, start_date, end_date) { where("#{self.date_builder("#{self.table_name}.#{column}")} BETWEEN ? AND ?", start_date, end_date) }
-    scope :before_or_equal_to_date, -> (column, date) { where("#{self.date_builder("#{self.table_name}.#{column}")} <= ?", date) }
-    scope :after_or_equal_to_date, -> (column, date) { where("#{self.date_builder("#{self.table_name}.#{column}")} >= ?", date) }
-    scope :at_time, -> (column, time) { where("#{self.time_builder("#{self.table_name}.#{column}")} LIKE ?", "%#{time}%") }
-    scope :mysql_json_contains, ->(column, value) {"JSON_EXTRACT(#{column}, '$.\"Request-ID\"') LIKE \"%#{value}%\""}
 
+  ActiveRecord::Base.class_eval do
     def update_by_params(params, set_nil = true)
       ActiveRecord::Base.transaction do
         self.class.column_names.each do |c|
@@ -66,8 +43,18 @@ module DcidevActiveRecord
         self.save
       end
     end
-
-
   end
+
+  # ActiveRecord::Relation.instance_eval do
+  #   scope :between_date, -> (column, start_date, end_date) { where("#{self.date_builder("#{self.table_name}.#{column}")} BETWEEN ? AND ?", start_date, end_date) }
+  #   scope :before_or_equal_to_date, -> (column, date) { where("#{self.date_builder("#{self.table_name}.#{column}")} <= ?", date) }
+  #   scope :after_or_equal_to_date, -> (column, date) { where("#{self.date_builder("#{self.table_name}.#{column}")} >= ?", date) }
+  #   scope :at_time, -> (column, time) { where("#{self.time_builder("#{self.table_name}.#{column}")} LIKE ?", "%#{time}%") }
+  #   scope :mysql_json_contains, ->(column, key, value) {"JSON_EXTRACT(#{column}, '$.\"#{key}\"') LIKE \"%#{value}%\""}
+
+
+
+
+  # end
 
 end
