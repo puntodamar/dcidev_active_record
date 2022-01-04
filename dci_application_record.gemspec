93369b999e888a15462648ea9d38199d9dd90b5d
puntodamar <punto@privyid.tech>
2022-01-04 16:06:30 +0700

    Init

---
+++ dci_application_record.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+    s.name = "dci_application_record"
+    s.version = "0.0.1"
+    # s.date = ""
+    s.summary = "This gem extends ApplicationRecord features"
+    s.description = "Testing phase"
+    s.authors = ["Punto Damar P"]
+    s.email = ["punto@privyid.tech"]
+    s.files = Dir.glob('{lib}/**/*.rb')
+    s.require_paths = ["lib"]
+  end
---
+++ lib/update_by_params.rb
@@ -0,0 +1,30 @@
+require 'application_record'
+require 'active_record'
+
+ApplicationRecord.class_eval do
+    def update_by_params(params, set_nil = true)
+        ActiveRecord::Base.transaction do
+          self.class.column_names.each do |c|
+            begin
+              if set_nil
+                eval("self.#{c} = params[:#{c.to_sym}]") if params.key?(c.to_sym)
+                eval("self.#{c} = params['#{c}']") if params.key?(c)
+              else
+                eval("self.#{c} = params[:#{c.to_sym}]") if params.key?(c.to_sym) && params[c.to_sym] != nil
+                eval("self.#{c} = params['#{c}']") if params.key?(c) && params[c] != nil
+              end
+            rescue IOError
+              raise "Tidak dapat menyimpan file#{c}"
+            end
+          end
+          params.select{|k, _| !k.is_a?(Symbol) && k.include?("_attributes")}.each do |k, _|
+            eval("self.#{k} = params[:#{k.to_sym}]")
+          end
+          self.save
+        end
+      end
+
+      def test
+        p "from gem"
+      end
+end
