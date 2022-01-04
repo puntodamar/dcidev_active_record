6b647f490d5cafad45825b24d13dbeec57d575a1
puntodamar <punto.damar@ti.ukdw.ac.id>
2022-01-04 22:53:29 +0700

    add scope

--- lib/dcidev_active_record.rb
+++ lib/dcidev_active_record.rb
@@ -2,6 +2,14 @@ require 'active_record'
 
 module DcidevActiveRecord
   ActiveRecord.class_eval do
+
+    scope :between_date, -> (column, start_date, end_date) { where("#{self.date_builder("#{self.table_name}.#{column}")} BETWEEN ? AND ?", start_date, end_date) }
+    scope :before_or_equal_to_date, -> (column, date) { where("#{self.date_builder("#{self.table_name}.#{column}")} <= ?", date) }
+    scope :after_or_equal_to_date, -> (column, date) { where("#{self.date_builder("#{self.table_name}.#{column}")} >= ?", date) }
+    scope :at_time, -> (column, time) { where("#{self.time_builder("#{self.table_name}.#{column}")} LIKE ?", "%#{time}%") }
+    scope :mysql_json_contains,(column, value) -> {"JSON_EXTRACT(#{column}, '$.\"Request-ID\"') LIKE \"%#{value}%\""}
+
+
     def update_by_params(params, set_nil = true)
         ActiveRecord::Base.transaction do
           self.class.column_names.each do |c|
